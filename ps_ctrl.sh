8198376227ef05fcf5ead2791542e7bc3e244682
Me1onRind <320314319@qq.com>
2021-01-03 00:29:36 +0800

    save

--- ps_ctrl.sh
+++ ps_ctrl.sh
@@ -6,11 +6,8 @@ action=$2
 case $process_name in
     mysql)
         if [ "$action" == "start" ]; then
-            mysqld_safe --defaults-file=/etc/my.cnf --user=root &
+            mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=mysql &
         elif [ "$action" == "stop" ]; then
-            mysqladmin -uroot -pguapi123 shutdown
-        elif [ "$action" == "restart" ]; then
-            mysqld_safe --defaults-file=/etc/my.cnf --user=root &
             mysqladmin -uroot -pguapi123 shutdown
         else
             echo "stupid"
